f4c610024dd2393e90ceadb3eca0c036e72ab8a0
Chefmanaphy <chefmanaphy@hotmail.fr>
2018-10-08 11:49:42 +0200

    repaire en +

--- PSI/main.py
+++ PSI/main.py
@@ -17,7 +17,7 @@ pygame.display.set_caption("Projet SI scanner3d")
 
 lss = 0
 theta = 0
-focal = 200
+focal = 400
 
 
 class Drawing:
@@ -39,12 +39,13 @@ class Drawing:
 
 	def ellipse(pos,color,radius):
 		pos = Drawing.apply3dstuff(pos)
-		pygame.draw.circle(screen,color,(int(pos.x),int(pos.y)),radius,0)
+		pygame.draw.circle(screen,color,(int(pos.x),height-int(pos.y)),radius,0)
 
 	def line(posa, posb, color, strokeweight):
 		posa = Drawing.apply3dstuff(posa)
 		posb = Drawing.apply3dstuff(posb)
-		pygame.draw.line(screen, color, [int(posa.x),int(posa.y)], [int(posb.x),int(posb.y)],strokeweight)
+		pygame.draw.line(screen, color, [int(posa.x),height-int(posa.y)], [int(posb.x),height-int(posb.y)],strokeweight)
+
 
 
 def main():
@@ -75,14 +76,23 @@ def draw():
 				points.append(Vector([i*100-50,j*100-50,k*100-50]))
 
 	for i in range(len(points)):
-		points[i] = points[i].rotate(theta,theta,theta)
+		points[i] = points[i].rotate(sin(theta*4)/6,theta,0)
 		points[i] = points[i].translate(Vector([0,0,200]))
 
-	for point in points:
-		Drawing.ellipse(point,[0,0,0],3)
+	#for point in points:
+	#	Drawing.ellipse(point,[0,0,0],3)
 
 	def connect(i,j):
 		Drawing.line(points[i],points[j],[0,0,0],2)
+
+	origin = Vector([0,0,0]).rotate(sin(theta*4)/6,theta,0).translate(Vector([0,-50,200]))
+	xpoint = Vector([10,0,0]).rotate(sin(theta*4)/6,theta,0).translate(Vector([0,-50,200]))
+	ypoint = Vector([0,10,0]).rotate(sin(theta*4)/6,theta,0).translate(Vector([0,-50,200]))
+	zpoint = Vector([0,0,10]).rotate(sin(theta*4)/6,theta,0).translate(Vector([0,-50,200]))
+	Drawing.line(origin,xpoint,[255,0,0],2)
+	Drawing.line(origin,ypoint,[0,255,0],2)
+	Drawing.line(origin,zpoint,[0,0,255],2)
+
 	for i in range(2):
 		connect(i*4+0,i*4+1)
 		connect(i*4+2,i*4+3)
